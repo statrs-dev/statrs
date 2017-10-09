ba4c859d3089107b15436abf336868503599a58a
Mikhail Pak <mikhail.pak@tum.de>
2017-10-09 16:58:58 +0200

    refactor: test the std::f64 implementation of exp_m1

--- src/function/stable.rs
+++ src/function/stable.rs
@@ -10,23 +10,7 @@ use std::f64;
 ///
 /// Returns `f64::NAN` if `pow` is `f64::NAN`
 pub fn exp_minus_one(pow: f64) -> f64 {
-    if pow.is_nan() {
-        return f64::NAN;
-    }
-
-    let x = pow.abs();
-    if x > 0.1 {
-        pow.exp() - 1.0
-    } else {
-        let mut k = 0;
-        let mut term = 1.0;
-        internal::series(|| {
-            k += 1;
-            term *= pow;
-            term /= k as f64;
-            term
-        })
-    }
+    pow.exp_m1()
 }
 
 #[cfg_attr(rustfmt, rustfmt_skip)]
@@ -40,7 +24,7 @@ mod test {
         assert_eq!(super::exp_minus_one(f64::INFINITY), f64::INFINITY);
         assert_eq!(super::exp_minus_one(f64::NEG_INFINITY), -1.0);
         assert_eq!(super::exp_minus_one(-50.0), -0.99999999999999999999980712501520360822169826571834729874);
-        assert_eq!(super::exp_minus_one(-2.5), -0.91791500137610120483047132553284019216219587898456);
+        assert_almost_eq!(super::exp_minus_one(-2.5), -0.91791500137610120483047132553284019216219587898456, 1e-15);
         assert_almost_eq!(super::exp_minus_one(-0.1), -0.09516258196404042683575094055356337880529463901959, 1e-8);
         assert_eq!(super::exp_minus_one(0.0), 0.0);
         assert_almost_eq!(super::exp_minus_one(0.1), 0.1051709180756476248117078264902466682245471947375187, 1e-8);
