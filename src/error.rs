statrs-dev/statrs

commit 4d9451398987a28aed09ea4303dd5380fcfbbac6

Add test asserting that `StatsError` is Sync & Send

src/error.rs
@@ -104,3 +104,18 @@ impl fmt::Display for StatsError {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_sync<T: Sync>() {}
+    fn assert_send<T: Send>() {}
+
+    #[test]
+    fn test_sync_send() {
+        // Error types should implement Sync and Send
+        let _ = assert_sync::<StatsError>();
+        let _ = assert_send::<StatsError>();
+    }
+}

commit 6ac1fc7cd7fbda83d9939a8571c6e1dfa928ccb5

Fix clippy error introduced in #226

src/error.rs
@@ -115,7 +115,7 @@ mod tests {
     #[test]
     fn test_sync_send() {
         // Error types should implement Sync and Send
-        let _ = assert_sync::<StatsError>();
-        let _ = assert_send::<StatsError>();
+        assert_sync::<StatsError>();
+        assert_send::<StatsError>();
     }
 }
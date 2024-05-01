6ac1fc7cd7fbda83d9939a8571c6e1dfa928ccb5
FreezyLemon <contact@freezylemon.com>
2024-05-01 22:26:42 +0200

    Fix clippy error introduced in #226

--- src/error.rs
+++ src/error.rs
@@ -115,7 +115,7 @@ mod tests {
     #[test]
     fn test_sync_send() {
         // Error types should implement Sync and Send
-        let _ = assert_sync::<StatsError>();
-        let _ = assert_send::<StatsError>();
+        assert_sync::<StatsError>();
+        assert_send::<StatsError>();
     }
 }
