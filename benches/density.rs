2476696c8bf33819edbf401014a215d0487e7720
Georges SARR <georgesmbissanes@gmail.com>
2025-07-20 22:43:02 +0200

    chore: density functions borrowing evaluation pointS instead of owning them.

--- benches/density.rs
+++ benches/density.rs
@@ -11,32 +11,34 @@ where
 {
     (0..n_samples).map(|_| rand::random()).collect()
 }
+
 fn bench_density(c: &mut Criterion) {
     let samples = generate(100_00);
-    // generate_1d(100_000);
     let mut group = c.benchmark_group("density");
     group.bench_function("knn_density_1d", |b| {
         b.iter(|| {
-            let _f = statrs::density::knn::knn_pdf([0.], &samples, None);
+            let _f = statrs::density::knn::knn_pdf(&[0.], &samples, None);
         });
     });
+
     let samples = generate(100_000);
     group.bench_function("knn_density_3d", |b| {
         b.iter(|| {
-            let _f = statrs::density::knn::knn_pdf([0., 0., 0.], &samples, None);
+            let _f = statrs::density::knn::knn_pdf(&[0., 0., 0.], &samples, None);
         });
     });
 
     let samples = generate(100_000);
     group.bench_function("kde_density_1d", |b| {
         b.iter(|| {
-            let _f = statrs::density::kde::kde_pdf(Vector1::new(0.), &samples, None);
+            let _f = statrs::density::kde::kde_pdf(&Vector1::new(0.), &samples, None);
         });
     });
+
     let samples = generate(100_000);
     group.bench_function("kde_density_3d", |b| {
         b.iter(|| {
-            let _f = statrs::density::kde::kde_pdf(Vector3::new(0., 0., 0.), &samples, None);
+            let _f = statrs::density::kde::kde_pdf(&Vector3::new(0., 0., 0.), &samples, None);
         });
     });
 }
