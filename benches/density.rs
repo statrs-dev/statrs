b88259da186045e2ddcde94cd1d11feacecbb752
Georges SARR <georgesmbissanes@gmail.com>
2025-11-17 03:44:34 +0100

    fix imports and add some tests.

--- benches/density.rs
+++ benches/density.rs
@@ -1,19 +1,19 @@
 extern crate criterion;
 extern crate rand;
 extern crate statrs;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use nalgebra::{Vector1, Vector3};
-use rand::distributions::Standard;
+use rand::distr::StandardUniform;
 
 fn generate<T>(n_samples: usize) -> Vec<T>
 where
-    Standard: rand::distributions::Distribution<T>,
+    StandardUniform: rand::distr::Distribution<T>,
 {
     (0..n_samples).map(|_| rand::random()).collect()
 }
 
 fn bench_density(c: &mut Criterion) {
-    let samples = generate(100_00);
+    let samples = generate(100_000);
     let mut group = c.benchmark_group("density");
     group.bench_function("knn_density_1d", |b| {
         b.iter(|| {
