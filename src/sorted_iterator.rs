2341511167369bd394c15d2225ff97aec26039d2
AshrafIbrahim03 <btd2eb@virginia.edu>
2026-08-21 14:41:57 -0400

    Renamed and added derive for sorted iterator types

--- src/sorted_iterator.rs
+++ src/sorted_iterator.rs
@@ -1,30 +1,28 @@
-use std::marker::PhantomData;
-
-use crate::stats_tests::NaNPolicy;
-
-pub trait SortedIterator {
-    fn sorted_iter(&self, policy: NaNPolicy) -> Sorted;
+pub enum SortedIteratorError {
+    TestError,
+}
+pub trait IntoSortedIterator {
+    fn into_sorted_iter(&self) -> Sorted;
 }
 
-impl SortedIterator for Vec<f64> {
-    fn sorted_iter(&self, policy: NaNPolicy) -> Sorted {
-        Sorted::new(self, policy)
+impl IntoSortedIterator for Vec<f64> {
+    fn into_sorted_iter(&self) -> Sorted {
+        Sorted::new(self)
     }
 }
 
 /// TODO iron out implementation details later because this is not optimal retrieval of sorted data
+#[derive(Clone)]
 pub struct Sorted {
     sorted_iter: std::vec::IntoIter<f64>,
-    policy: NaNPolicy,
 }
 
 impl Sorted {
-    pub fn new(data: &[f64], policy: NaNPolicy) -> Self {
+    pub fn new(data: &[f64]) -> Self {
         let mut cloned = Vec::from(data);
         cloned.sort_by(|a, b| a.total_cmp(b));
         Self {
             sorted_iter: cloned.into_iter(),
-            policy,
         }
     }
 }
@@ -33,6 +31,9 @@ impl Iterator for Sorted {
     type Item = f64;
 
     fn next(&mut self) -> Option<Self::Item> {
+        ///TODO potential algorithm:
+        ///use bloom filter to test for untraveled indices, I'm not sure how to implement traversal
+        ///yet
         self.sorted_iter.next()
     }
 }
