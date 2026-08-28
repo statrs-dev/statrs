68c13e12a3720fd56d7120308dfa84a060552ba1
AshrafIbrahim03 <btd2eb@virginia.edu>
2026-08-28 10:16:54 -0400

    Added convenience methods for instantiating SortedCollection

--- src/sorted_collection.rs
+++ src/sorted_collection.rs
@@ -1,22 +1,43 @@
+use std::cmp::Ordering;
+
 pub enum SortError {
     NotSorted,
 }
 
+pub enum Collection<'a, T> {
+    Ref(&'a [T]),
+    Owned(Vec<T>),
+}
+
 pub struct SortedCollection<'a, T> {
-    sorted: &'a [T],
+    sorted: Collection<'a, T>,
 }
 
 impl<'a, T> SortedCollection<'a, T>
 where
-    T: PartialOrd,
+    T: PartialOrd + Ord,
 {
-    pub fn new(coll: &'a [T]) -> Result<Self, SortError> {
-        match coll.windows(2).all(|sl| sl[0] < sl[1]) {
-            true => Ok(Self { sorted: coll }),
+    pub fn from_slice(coll: &'a [T]) -> Result<Self, SortError> {
+        match coll
+            .windows(2)
+            .all(|sl| sl[0].cmp(&sl[1]) == Ordering::Less)
+        {
+            true => Ok(Self {
+                sorted: Collection::Ref(coll),
+            }),
             false => Err(SortError::NotSorted),
         }
     }
-    pub fn iter(&self) -> std::slice::Iter<'a, T> {
-        self.sorted.iter()
+    pub fn from_mut_vec(mut coll: Vec<T>) -> Result<Self, SortError> {
+        coll.sort();
+        Ok(Self {
+            sorted: Collection::Owned(coll),
+        })
+    }
+    pub fn iter(&'a self) -> std::slice::Iter<'a, T> {
+        match self.sorted {
+            Collection::Ref(items) => items.iter(),
+            Collection::Owned(ref items) => items.iter(),
+        }
     }
 }
